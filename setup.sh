552d6c8090e7ce2a34781efd4748999385e7917a
darkinzen <h.bilgic1818@gmail.com>
2025-03-09 13:30:06 +0300

    delete device trees before cloning

--- setup.sh
+++ setup.sh
@@ -2,17 +2,24 @@
 
 ##CLONE DT
 ## mars 
+rm -rf device/xiaomi/mars
 git clone https://github.com/darkinzen/android_device_xiaomi_mars.git device/xiaomi/mars
+
 ## sm8350-common
+rm -rf device/xiaomi/sm8350-common
 git clone https://github.com/darkinzen/android_device_xiaomi_sm8350-common.git device/xiaomi/sm8350-common
 
 ## CLONE VENDOR FILES
 ## mars
+rm -rf vendor/xiaomi/mars
 git clone https://github.com/darkinzen/proprietary_vendor_xiaomi_mars.git vendor/xiaomi/mars
+
 ## sm8350-common
+rm -rf vendor/xiaomi/sm8350-common
 git clone https://github.com/darkinzen/proprietary_vendor_xiaomi_sm8350-common.git vendor/xiaomi/sm8350-common
 
 ##CLONE HARDWARE
+rm -rf hardware/xiaomi
 git clone https://github.com/darkinzen/android_hardware_xiaomi.git hardware/xiaomi
 
 ##CLONE KERNEL
